43e4555eeff50caf924d6b4f6a048dfad24e2654
NishanHolla <92212948+NishanHolla@users.noreply.github.com>
2023-04-18 19:34:28 +0530

    replaced href tags

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import './App.css'
 import Login from "./components/login/login.js"
 import Register from "./components/register/register.js"
 import MvApp from './components/homepage/Movies-App'
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route,Link } from "react-router-dom";
 import { useState } from 'react';
 import ReactDOM from 'react-dom/client';
 import background from './components/ticketsnatcher.png';
@@ -11,20 +11,20 @@ function Appbar(){
   return(
     <div style={{display:"block",justifyContent:'space-between',alignItems:"-moz-initial"}}>
       <nav class="navbar navbar-expand-lg navbar-light bg-danger" style={{height:"70px"}}>
-        <a class="navbar-brand mx-3 mt-3" href="http://localhost:3000/home"><p id="title" style={{color:"black" ,fontSize:"30px",fontFamily:"Impact"}}>ticketsnatcher</p></a>
+        <Link to="/home" class="navbar-brand mx-3 mt-3"><p id="title" style={{color:"black" ,fontSize:"30px",fontFamily:"Impact"}}>ticketsnatcher</p></Link>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse" id="navbarNav">
           <ul class="navbar-nav mx-3">
             <li class="nav-item">
-              <a class="nav-link" href="http://localhost:3000/home" id="home" style={{}}>Home</a>
+              <Link to="/home" class="nav-link" id="home">Home</Link>
             </li>
             <li class="nav-item">
-              <a class="nav-link" href="http://localhost:3000/login" id="login">Login</a>
+              <Link to="/login" class="nav-link" id="login">Login</Link>
             </li>
             <li class="nav-item">
-              <a class="nav-link" href="http://localhost:3000/register" id="regis">Register</a>
+              <Link to="/register" class="nav-link" id="regis">Register</Link>
             </li>
           </ul>
         </div>
